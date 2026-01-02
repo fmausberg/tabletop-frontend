c02403d517f177cd1acddeb1e8a5f194e6a9419c
fmausberg <felix.mausberg@gmail.com>
2026-01-02 22:45:01 +0100

    cleaned

--- src/types/event.ts
+++
@@ -1,8 +0,0 @@
-export interface Event {
-    id: number;
-    name: string;
-    begin: string; // ISO date string
-    end: string; // ISO date string
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-}
